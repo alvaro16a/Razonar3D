d7d9b68f5db70cd816b62fa54906a876df7dd2ae
Alvaro Mena <alvaro16a@gmail.com>
2022-03-11 20:40:29 -0500

    Se creo el identificador ImpresoraID, se configuro directamente para poder ingresarle el de forma manual en caso de ser deseado, util en las pruebas unitarias

---
+++ src/main/java/domain/granja/value/ImpresoraID.java
@@ -0,0 +1,16 @@
+package domain.granja.value;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class ImpresoraID extends Identity {
+
+    private ImpresoraID(String valor){ //este constructor privado le asigna el valor al id
+        super(valor);
+    }
+
+    public ImpresoraID(){} //Este permite que se cree el elemento
+    public static ImpresoraID of(String valor) {
+        return new ImpresoraID(valor);
+    }
+
+}
