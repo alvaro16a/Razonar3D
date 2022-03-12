00ac0166ec5b42debc1921f9cc58157bfcc0fe5e
Alvaro Mena <alvaro16a@gmail.com>
2022-03-11 19:18:50 -0500

    Se creo el objeto de valor Persona, el cual define las propiedades basicas que identifican a una persona nombre,apellido y documento

---
+++ src/main/java/domain/value/Persona.java
@@ -0,0 +1,52 @@
+package domain.value;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Persona implements ValueObject<Persona.Props> {
+
+    private final String nombre;
+    private final String apellido;
+    private final String  documento;
+
+    public Persona(String nombre, String apellido, String documento) {
+        this.nombre = Objects.requireNonNull(nombre);
+        this.apellido = Objects.requireNonNull(apellido);
+        this.documento = Objects.requireNonNull(documento);
+    }
+
+    public Persona() {
+        this.nombre = "Name";
+        this.apellido = "LastName";
+        this.documento = "document";
+    }
+
+    @Override
+    public Props value() {
+
+        return new Props() {
+            @Override
+            public String nombre() {
+                return nombre;
+            }
+
+            @Override
+            public String apellido() {
+                return apellido;
+            }
+
+            @Override
+            public String documento() {
+                return documento;
+            }
+        };
+    }
+    
+    public interface Props{
+        String nombre();
+        String apellido();
+        String documento();
+    }
+
+}
