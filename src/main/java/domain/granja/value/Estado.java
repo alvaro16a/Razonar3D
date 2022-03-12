ca991eafcd22cf9de4540753581da4915e232bb6
Alvaro Mena <alvaro16a@gmail.com>
2022-03-11 19:54:48 -0500

    Se creo el objeto de valor Estado, el cual define si la impresora esta imprimiendo, apagada en mantenimiento o averiada

---
+++ src/main/java/domain/granja/value/Estado.java
@@ -0,0 +1,24 @@
+package domain.granja.value;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class Estado implements ValueObject<Estado.Operacional> {
+
+    private final Operacional value;
+
+    public Estado(Operacional value){
+        this.value =value;
+    }
+    public Estado(){
+        this.value =Operacional.APAGADA;
+    }
+
+    @Override
+    public Operacional value(){
+        return value;
+    }
+
+    public enum Operacional{
+        IMPRIMIENDO, APAGADA, MANTENIMIENTO, AVERIADO
+    }
+}
